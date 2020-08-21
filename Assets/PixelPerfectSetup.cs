5ea8e585d0d5366c89f118b4d5dace103fe1526a
Jose Neto <zenetoshl@gmail.com>
2020-08-21 12:21:44 -0300

    pixel perfect settings

---
+++ Assets/PixelPerfectSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectSetup : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.U2D.PixelPerfectRendering.pixelSnapSpacing =  camera.orthographicSize * 2.0f / camera.pixelHeight;
+    }
+}
