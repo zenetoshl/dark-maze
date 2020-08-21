df5c62bad2b6a43270efdd7bf7853d91705a032c
Jose Neto <zenetoshl@gmail.com>
2020-08-21 10:51:04 -0300

    adição dos renderers e teste de luzes

---
+++ Assets/FireEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class FireEffect : MonoBehaviour
+{
+
+    public Light2D light2d;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        light2d.pointLightOuterRadius = 5.5f + (Mathf.Cos(Time.time));
+    }
+}
